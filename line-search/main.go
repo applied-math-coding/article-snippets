7cdccfa644e0efff5e6dafaf5497e29242f900b3
Your Name <you@example.com>
2021-05-11 16:29:01 +0300

    line search impl

--- line-search/main.go
+++ line-search/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("test")
+	x := LineSearch(f, -5, 10, 0.0001)
+	fmt.Println(x, f(x))
+}
+
+func f(x float64) float64 {
+	return x * x
+}
+
+func LineSearch(f func(x float64) float64, a float64, b float64, acc float64) float64 {
+	q := 3.0/2.0 - math.Sqrt(5.0)/2.0
+	c := a + (b-a)*q
+	d := b - (b-a)*q
+	for math.Abs(b-a) > acc {
+		if f(c) < f(d) {
+			b = d
+			d = c
+			c = a + (b-a)*q
+		} else {
+			a = c
+			c = d
+			d = b - (b-a)*q
+		}
+	}
+	return a
 }
