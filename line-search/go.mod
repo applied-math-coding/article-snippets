01f3d38590873ddba38584f343de35b782c759ed
Your Name <you@example.com>
2021-05-18 13:33:14 +0300

    line search

--- line-search/go.mod
+++ line-search/go.mod
@@ -1,3 +1,5 @@
 module github.com/applied-math-coding/article-snippets/line-search
 
 go 1.16
+
+replace github.com/applied-math-coding/article-snippets/line-search => ./
--- line-search/main.go
+++ line-search/main.go
@@ -2,32 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math"
+
+	lineSearch "github.com/applied-math-coding/article-snippets/line-search/linesearch"
 )
 
 func main() {
-	x := LineSearch(f, -5, 10, 0.0001)
+	x := lineSearch.LineSearch(f, -5, 10, 0.0001)
 	fmt.Println(x, f(x))
 }
 
 func f(x float64) float64 {
 	return x * x
-}
-
-func LineSearch(f func(x float64) float64, a float64, b float64, acc float64) float64 {
-	q := 3.0/2.0 - math.Sqrt(5.0)/2.0
-	c := a + (b-a)*q
-	d := b - (b-a)*q
-	for math.Abs(b-a) > acc {
-		if f(c) < f(d) {
-			b = d
-			d = c
-			c = a + (b-a)*q
-		} else {
-			a = c
-			c = d
-			d = b - (b-a)*q
-		}
-	}
-	return a
 }
